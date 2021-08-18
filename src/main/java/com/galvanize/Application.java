2a432cd1999ab0de9ea228d331b2462dfa1eba4e
Ben Hunter <learntocode@benhunter.me>
2021-08-18 11:58:54 -0500

    Using Gradle to build.

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -3,5 +3,10 @@ package com.galvanize;
 public class Application {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println(Application.constString());
+    }
+
+    public static String constString() {
+        return "CONSTANT";
     }
 }
